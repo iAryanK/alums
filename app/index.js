e9bf29cfc98a8f753d221cb51dfbd92265c47f54
Aryan <aryaninmail@gmail.com>
2024-04-17 20:23:59 +0530

    onboarding screen created

--- app/index.js
+++ app/index.js
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
+import { router } from 'expo-router';
 
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl font-pblack">Alums</Text>
-            <StatusBar style="auto" />
-        </View>
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView contentContainerStyle={{ height: '100%' }}>
+                <View className='w-full justify-center items-center h-full px-4'>
+                    <Image
+                        source={images.logo}
+                        className='w-[130px] h-[84px]'
+                        resizeMode='contain'
+                    />
+                    <Image
+                        source={images.cards}
+                        className='max-w-[380px] h-[300px] w-full'
+                        resizeMode='contain'
+                    />
+
+                    <View className='relative mt-5'>
+                        <Text className='text-3xl text-white font-bold text-center'>
+                            Explore your Campus family with {' '}
+                            <Text className="text-secondary-200">Alums</Text>
+                        </Text>
+
+                        <Image
+                            source={images.path}
+                            className='w-136px h-[15px] absolute -bottom-1 -right-10'
+                            resizeMode='contain'
+                        />
+
+                    </View>
+                    <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Increase networking among like minded people. Stay in touch with your campus with Alums</Text>
+
+                    <CustomButton
+                        title="Continue with email"
+                        handlePress={() => router.push('/sign-in')}
+                        containerStyles='w-full mt-7'
+                    />
+                </View>
+            </ScrollView>
+
+            <StatusBar backgroundColor='#161622'
+                style='light' />
+
+        </SafeAreaView>
     );
 }
