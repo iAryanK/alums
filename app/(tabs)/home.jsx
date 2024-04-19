9d237e7b7ae7a9ff4f8c2e7fea4b76407dbc8c74
Aryan <aryaninmail@gmail.com>
2024-04-19 08:32:02 +0530

    home feed implemented

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,14 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, Image, RefreshControl, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
+import SearchInput from '../../components/SearchInput'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
+import { getAllPosts } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import PostCard from '../../components/PostCard'
 
 const Home = () => {
+    const { data: posts, refetch } = useAppwrite(getAllPosts);
+
+    const [refreshing, setRefreshing] = useState(false)
+    const onRefresh = async () => {
+        setRefreshing(true);
+        await refetch();
+        setRefreshing(false);
+    }
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView className="bg-primary h-full">
+            <FlatList
+                data={posts}
+                keyExtractor={(item) => item.$id}
+                renderItem={({ item }) => (
+                    <PostCard
+                        post={item}
+                    />
+                )}
+                ListHeaderComponent={() => (
+                    <View className="my-6 px-4 space-y-6">
+                        <View className="justify-between items-start flex-row mb-6">
+                            <View>
+                                <Text className="font-pmedium text-sm text-gray-100">Welcome Back</Text>
+                                <Text className="text-2xl font-psemibold text-gray-100">JSMastery</Text>
+                            </View>
+
+                            <View className="mt-1.5">
+                                <Image
+                                    source={images.logoSmall}
+                                    className="w-9 h-10"
+                                    resizeMode='contain'
+                                />
+                            </View>
+                        </View>
+
+                        <SearchInput />
+
+                        <View className="w-full flex-1 pt-5 pb-8">
+                            <Text className="text-green-100 text-lg font-pregular mb-3">
+                                Latest Videos
+                            </Text>
+
+                            <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+                        </View>
+                    </View>
+                )}
+                ListEmptyComponent={() => (
+                    <EmptyState
+                        title="No posts found"
+                        subtitle="Be the first one to upload a post"
+                    />
+                )}
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+            />
+        </SafeAreaView>
     )
 }
 
 export default Home
-
-const styles = StyleSheet.create({})
